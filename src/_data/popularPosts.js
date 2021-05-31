0970293aa1a7fa8bd45ef043e2f24f18895de084
Pavel Dolgov <gotterbild23@gmail.com>
2021-05-31 20:25:03 +0300

    Add sidebar & README

---
+++ src/_data/popularPosts.js
@@ -0,0 +1,27 @@
+module.exports = [
+	{
+		url: '/raketnye-pechi-i-verhniy-rozjig',
+		title: 'Отопление. Печи. Ракетные печи и верхний розжиг',
+		image: 'Raket.jpg'
+	},
+	{
+		url: '/razmyshlenija-vladelca-doma-iz-meshkov',
+		title: 'Размышления владельца дома из мешков',
+		image: '2014-05-04-16-19-02.jpg'
+	},
+	{
+		url: '/otoplenie-pechi-vneshniy-pritok-i-vtorichniy-vozduh',
+		title: 'Отопление. Печи. Внешний приток и вторичный воздух',
+		image: 'Palamisprosessi2006.jpg'
+	},
+	{
+		url: '/glina-i-glinobeton',
+		title: 'Глина и глинобетон. Удивительные свойства и их применение',
+		image: 'glinobeton.jpg'
+	},
+	{
+		url: '/otoplenie-pechi-kolpakovye-i-dvuhkolpakovye',
+		title: 'Отопление. Печи. Колпаковые и двухколпаковые',
+		image: 'pechi_p061.jpg'
+	},
+]
